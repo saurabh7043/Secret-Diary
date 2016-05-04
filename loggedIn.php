41f41bb3ab498833ae7acd39bffe079095c2af56
saurabh7043 <saurabh7043@gmail.com>
2016-05-03 22:29:32 -0700

    Secret-Diary App

---
+++ loggedIn.php
@@ -0,0 +1,6 @@
+<?php
+
+	setcookie("id","",time()+3600);
+	echo $_COOKIE['id'];
+
+?>
---
+++ project/connection.php
@@ -0,0 +1,5 @@
+<?php
+
+	$link=mysqli_connect("localhost","cl59-example-jqx","NtF/nzrxr","cl59-example-jqx");
+
+?>
